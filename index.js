5147346eccd55ae467add39130d10e845a15e797
Gefen <gefenbar009@gmail.com>
2022-10-18 22:58:55 +0300

    build 1.0

--- index.js
+++ index.js
@@ -197,18 +197,14 @@ circlesOdd.forEach(el => { el.style.animation = "bounceUp 3s infinite" })
 // 
 let moon = document.getElementById("moon_image")
 window.addEventListener('scroll', () => {
-  if (!light_mod) {
-    let value = window.scrollY
-    moon.style.top = value * 1.15 + "px"
-  }
+  let value = window.scrollY
+  moon.style.top = value * 1.15 + "px"
 }
 )
 
 let sun = document.getElementById("sun_image")
 window.addEventListener('scroll', () => {
-  if (light_mod) {
-    let value = window.scrollY
-    sun.style.bottom = value * 1.05 + "px"
-  }
+  let value = window.scrollY
+  sun.style.bottom = value * 1.05 + "px"
 })
 // 
