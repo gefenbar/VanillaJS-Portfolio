d4171d6024bc01d02001b5019d7dcb5a7b9b9b42
Gefen <gefenbar009@gmail.com>
2023-07-27 18:14:00 +0300

    view count fix

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -1,27 +1,22 @@
+let viewCount = Number(process.env.VIEW_COUNT);
+
 exports.handler = async function (event, context) {
-  if (event.httpMethod !== 'GET') {
+  if (event.httpMethod === 'GET') {
+    viewCount++;
+    process.env.VIEW_COUNT = viewCount.toString();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ viewCount }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    };
+  } else {
     return {
       statusCode: 405,
       body: JSON.stringify({ message: 'Method Not Allowed' }),
     };
   }
-
-  // Retrieve the view count from the environment variable
-  let viewCount = process.env.VIEW_COUNT ? parseInt(process.env.VIEW_COUNT) : 0;
-
-  // Increment the view count
-  viewCount++;
-
-  // Update the environment variable with the new view count
-  process.env.VIEW_COUNT = viewCount;
-
-  // Return the updated view count in the response
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ viewCount }),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  };
 };
