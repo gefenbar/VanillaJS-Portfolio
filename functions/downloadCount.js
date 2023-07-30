c97888215e941bc7c04f751a10c8464fbdf1c18d
Gefen <gefenbar009@gmail.com>
2023-07-30 20:20:10 +0300

    view and download counter

--- functions/downloadCount.js
+++ functions/downloadCount.js
@@ -1,31 +1,44 @@
 const db = require('./db');
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === "GET") {
-    db.get('SELECT count FROM counts WHERE name = ?', 'downloadCount', (err, row) => {
-      if (err) {
-        console.error('Error reading downloadCount:', err);
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Error reading downloadCount' }),
-        };
-      } else {
-        const downloadCount = row.count;
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ downloadCount }),
-        };
-      }
-    });
-  } else {
-    db.run('UPDATE counts SET count = count + 1 WHERE name = ?', 'downloadCount', (err) => {
-      if (err) {
-        console.error('Error updating downloadCount:', err);
-      }
-    });
+  try {
+    if (event.httpMethod === 'GET') {
+      db.get('SELECT count FROM counts WHERE name = ?', 'downloadCount', (err, row) => {
+        if (err) {
+          console.error('Error reading downloadCount:', err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error reading downloadCount' }),
+          };
+        } else {
+          const downloadCount = row.count;
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ downloadCount }),
+          };
+        }
+      });
+    } else {
+      db.run('UPDATE counts SET count = count + 1 WHERE name = ?', 'downloadCount', (err) => {
+        if (err) {
+          console.error('Error updating downloadCount:', err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error updating downloadCount' }),
+          };
+        } else {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Download count incremented successfully.' }),
+          };
+        }
+      });
+    }
+  } catch (error) {
+    console.error('Internal server error:', error);
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Download count incremented successfully." }),
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal server error' }),
     };
   }
 };
--- functions/viewCount.js
+++ functions/viewCount.js
@@ -1,31 +1,44 @@
 const db = require('./db');
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === "GET") {
-    db.get('SELECT count FROM counts WHERE name = ?', 'viewCount', (err, row) => {
-      if (err) {
-        console.error('Error reading viewCount:', err);
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Error reading viewCount' }),
-        };
-      } else {
-        const viewCount = row.count;
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ viewCount }),
-        };
-      }
-    });
-  } else {
-    db.run('UPDATE counts SET count = count + 1 WHERE name = ?', 'viewCount', (err) => {
-      if (err) {
-        console.error('Error updating viewCount:', err);
-      }
-    });
+  try {
+    if (event.httpMethod === 'GET') {
+      db.get('SELECT count FROM counts WHERE name = ?', 'viewCount', (err, row) => {
+        if (err) {
+          console.error('Error reading viewCount:', err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error reading viewCount' }),
+          };
+        } else {
+          const viewCount = row.count;
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ viewCount }),
+          };
+        }
+      });
+    } else {
+      db.run('UPDATE counts SET count = count + 1 WHERE name = ?', 'viewCount', (err) => {
+        if (err) {
+          console.error('Error updating viewCount:', err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error updating viewCount' }),
+          };
+        } else {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'View count incremented successfully.' }),
+          };
+        }
+      });
+    }
+  } catch (error) {
+    console.error('Internal server error:', error);
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "View count incremented successfully." }),
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal server error' }),
     };
   }
 };
