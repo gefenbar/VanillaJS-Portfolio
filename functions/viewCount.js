c8c68ba27d08c9cf26bedd8ceb2c8bd0e779a2ef
Gefen <gefenbar009@gmail.com>
2023-07-30 17:56:20 +0300

    view and download counter

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -2,31 +2,56 @@
 
 const fs = require('fs');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
-const viewCountFilePath = path.join(__dirname, 'data', 'viewCount.json');
+const dbFilePath = path.join(__dirname, 'data', 'viewCount.db');
 
-let viewCount;
+// Create and open the SQLite database connection
+const db = new sqlite3.Database(dbFilePath, (err) => {
+  if (err) {
+    console.error('Error opening database:', err);
+  } else {
+    console.log('Connected to the database.');
+  }
+});
 
-// Read the view count from the storage (e.g., a file) on server startup
-try {
-  const data = fs.readFileSync(viewCountFilePath, 'utf8');
-  const jsonData = JSON.parse(data);
-  viewCount = jsonData.viewCount || 0;
-} catch (err) {
-  console.error('Error reading view count file:', err);
-  viewCount = 0;
-}
+// Create a table to store the view count if it doesn't exist
+db.run(`
+  CREATE TABLE IF NOT EXISTS view_counts (
+    id INTEGER PRIMARY KEY,
+    count INTEGER DEFAULT 0
+  );
+`);
+
+let viewCount = 0;
+
+// Read the view count from the database on server startup
+db.get('SELECT count FROM view_counts WHERE id = 1', (err, row) => {
+  if (err) {
+    console.error('Error reading view count from the database:', err);
+  } else {
+    if (row) {
+      viewCount = row.count;
+    } else {
+      // Insert the initial view count into the database
+      db.run('INSERT INTO view_counts (id, count) VALUES (?, ?)', [1, viewCount], (err) => {
+        if (err) {
+          console.error('Error inserting initial view count:', err);
+        }
+      });
+    }
+  }
+});
 
 exports.handler = async function (event, context) {
   if (event.httpMethod === 'GET') {
     viewCount++;
-    // Update the view count in the storage (e.g., a file)
-    try {
-      const jsonData = { viewCount };
-      fs.writeFileSync(viewCountFilePath, JSON.stringify(jsonData), 'utf8');
-    } catch (err) {
-      console.error('Error updating view count:', err);
-    }
+    // Update the view count in the database
+    db.run('UPDATE view_counts SET count = ? WHERE id = ?', [viewCount, 1], (err) => {
+      if (err) {
+        console.error('Error updating view count in the database:', err);
+      }
+    });
 
     return {
       statusCode: 200,
