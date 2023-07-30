b98b1491e6967dde92de05f6911e3d9c29a2b941
Gefen <gefenbar009@gmail.com>
2023-07-30 17:51:47 +0300

    view and download counter

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -4,29 +4,17 @@ const fs = require('fs');
 const path = require('path');
 
 const viewCountFilePath = path.join(__dirname, 'data', 'viewCount.json');
-const downloadCountFilePath = path.join(__dirname, 'data', 'downloadCount.json');
 
 let viewCount;
-let downloadCount;
 
 // Read the view count from the storage (e.g., a file) on server startup
 try {
-  const viewCountData = fs.readFileSync(viewCountFilePath, 'utf8');
-  const viewCountJson = JSON.parse(viewCountData);
-  viewCount = viewCountJson.viewCount || 0;
+  const data = fs.readFileSync(viewCountFilePath, 'utf8');
+  const jsonData = JSON.parse(data);
+  viewCount = jsonData.viewCount || 0;
 } catch (err) {
   console.error('Error reading view count file:', err);
   viewCount = 0;
-}
-
-// Read the download count from the storage (e.g., a file) on server startup
-try {
-  const downloadCountData = fs.readFileSync(downloadCountFilePath, 'utf8');
-  const downloadCountJson = JSON.parse(downloadCountData);
-  downloadCount = downloadCountJson.downloadCount || 0;
-} catch (err) {
-  console.error('Error reading download count file:', err);
-  downloadCount = 0;
 }
 
 exports.handler = async function (event, context) {
@@ -34,8 +22,8 @@ exports.handler = async function (event, context) {
     viewCount++;
     // Update the view count in the storage (e.g., a file)
     try {
-      const viewCountJsonData = { viewCount };
-      fs.writeFileSync(viewCountFilePath, JSON.stringify(viewCountJsonData), 'utf8');
+      const jsonData = { viewCount };
+      fs.writeFileSync(viewCountFilePath, JSON.stringify(jsonData), 'utf8');
     } catch (err) {
       console.error('Error updating view count:', err);
     }
@@ -43,24 +31,6 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ viewCount }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-    };
-  } else if (event.httpMethod === 'POST') {
-    downloadCount++;
-    // Update the download count in the storage (e.g., a file)
-    try {
-      const downloadCountJsonData = { downloadCount };
-      fs.writeFileSync(downloadCountFilePath, JSON.stringify(downloadCountJsonData), 'utf8');
-    } catch (err) {
-      console.error('Error updating download count:', err);
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ downloadCount }),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
