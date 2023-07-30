35026d0f27f4c12682d169086963dd0dd5bd92e5
Gefen <gefenbar009@gmail.com>
2023-07-30 13:21:19 +0300

    view and download counter

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -9,7 +9,7 @@ let viewCount = 0;
 // Read the initial view count from the storage (e.g., a file) on server startup
 fs.readFile(viewCountFilePath, 'utf8', (err, data) => {
   if (!err) {
-    viewCount = Number(data);
+    viewCount = parseInt(data, 10); // Parse the data as an integer
   }
 });
 
@@ -29,17 +29,16 @@ exports.handler = async function (event, context) {
     fs.writeFile(viewCountFilePath, viewCount.toString(), 'utf8', (err) => {
       if (err) {
         console.error('Error updating view count:', err);
-      } else {
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ viewCount }),
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
-        };
       }
     });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ viewCount }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    };
   } else {
     return {
       statusCode: 405,
