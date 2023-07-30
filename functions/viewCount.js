e06dac71fbea8d8037515fcecff5fc37f6c11e22
Gefen <gefenbar009@gmail.com>
2023-07-30 15:16:31 +0300

    view and download counter

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -7,26 +7,26 @@ const viewCountFilePath = 'viewCount.json';
 let viewCount;
 
 // Read the view count from the storage (e.g., a file) on server startup
-fs.readFile(viewCountFilePath, 'utf8', (err, data) => {
-  if (!err) {
-    viewCount = parseInt(data, 10);
-    if (isNaN(viewCount)) {
-      viewCount = 0;
-    }
-  } else {
+try {
+  const data = fs.readFileSync(viewCountFilePath, 'utf8');
+  viewCount = parseInt(data, 10);
+  if (isNaN(viewCount)) {
     viewCount = 0;
   }
-});
+} catch (err) {
+  console.error('Error reading view count file:', err);
+  viewCount = 0;
+}
 
 exports.handler = async function (event, context) {
   if (event.httpMethod === 'GET') {
     viewCount++;
     // Update the view count in the storage (e.g., a file)
-    fs.writeFile(viewCountFilePath, viewCount.toString(), 'utf8', (err) => {
-      if (err) {
-        console.error('Error updating view count:', err);
-      }
-    });
+    try {
+      fs.writeFileSync(viewCountFilePath, viewCount.toString(), 'utf8');
+    } catch (err) {
+      console.error('Error updating view count:', err);
+    }
 
     return {
       statusCode: 200,
