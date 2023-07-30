8d0d17bbc54380b6ef43468c3e843af5d300a34f
Gefen <gefenbar009@gmail.com>
2023-07-30 17:51:14 +0300

    view and download counter

--- functions/viewCount.js
+++ functions/viewCount.js
@@ -1,26 +1,32 @@
 // serverless-function.js
 
 const fs = require('fs');
-const lockFile = require('lockfile');
+const path = require('path');
 
-const viewCountFilePath = 'viewCount.json';
-const lockFilePath = 'viewCount.lock';
+const viewCountFilePath = path.join(__dirname, 'data', 'viewCount.json');
+const downloadCountFilePath = path.join(__dirname, 'data', 'downloadCount.json');
 
-let viewCount = 0;
+let viewCount;
+let downloadCount;
 
 // Read the view count from the storage (e.g., a file) on server startup
 try {
-  lockFile.lockSync(lockFilePath); // Acquire the lock to prevent conflicts
-  const data = fs.readFileSync(viewCountFilePath, 'utf8');
-  viewCount = parseInt(data, 10);
-  if (isNaN(viewCount)) {
-    viewCount = 0;
-  }
+  const viewCountData = fs.readFileSync(viewCountFilePath, 'utf8');
+  const viewCountJson = JSON.parse(viewCountData);
+  viewCount = viewCountJson.viewCount || 0;
 } catch (err) {
   console.error('Error reading view count file:', err);
   viewCount = 0;
-} finally {
-  lockFile.unlockSync(lockFilePath); // Release the lock
+}
+
+// Read the download count from the storage (e.g., a file) on server startup
+try {
+  const downloadCountData = fs.readFileSync(downloadCountFilePath, 'utf8');
+  const downloadCountJson = JSON.parse(downloadCountData);
+  downloadCount = downloadCountJson.downloadCount || 0;
+} catch (err) {
+  console.error('Error reading download count file:', err);
+  downloadCount = 0;
 }
 
 exports.handler = async function (event, context) {
@@ -28,17 +34,33 @@ exports.handler = async function (event, context) {
     viewCount++;
     // Update the view count in the storage (e.g., a file)
     try {
-      lockFile.lockSync(lockFilePath); // Acquire the lock to prevent conflicts
-      fs.writeFileSync(viewCountFilePath, viewCount.toString(), 'utf8');
+      const viewCountJsonData = { viewCount };
+      fs.writeFileSync(viewCountFilePath, JSON.stringify(viewCountJsonData), 'utf8');
     } catch (err) {
       console.error('Error updating view count:', err);
-    } finally {
-      lockFile.unlockSync(lockFilePath); // Release the lock
     }
 
     return {
       statusCode: 200,
       body: JSON.stringify({ viewCount }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    };
+  } else if (event.httpMethod === 'POST') {
+    downloadCount++;
+    // Update the download count in the storage (e.g., a file)
+    try {
+      const downloadCountJsonData = { downloadCount };
+      fs.writeFileSync(downloadCountFilePath, JSON.stringify(downloadCountJsonData), 'utf8');
+    } catch (err) {
+      console.error('Error updating download count:', err);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ downloadCount }),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
